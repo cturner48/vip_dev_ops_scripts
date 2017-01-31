5078c464699f2ac366badb5fa76442a4624114d0
Chris Turner <cturner48@gatech.edu>
2017-01-30 20:55:26 -0500

    Development and testing scripts for smart cities infrastructure.

---
+++ build_chroot.sh
@@ -0,0 +1,113 @@
+
+
+## Adding user information for rootfs ##
+## Default password should be root ##
+## unless prompted otherwise. ##
+adduser --gecos "" ubuntu
+addgroup ubuntu adm
+addgroup ubuntu sudo
+#addgroup ubuntu i2c
+addgroup ubuntu audio
+addgroup ubuntu video
+addgroup ubuntu dialout
+sudo chown -R ubuntu.ubuntu /home/ubuntu
+rm /home/ubuntu/chroot.log
+
+
+## Adding repositories and updating apt-get package manager ##
+echo "deb http://ports.ubuntu.com/ trusty main restricted universe multiverse" > /etc/apt/sources.list
+echo "deb http://ports.ubuntu.com/ trusty-security main restricted universe multiverse" >> /etc/apt/sources.list
+echo "deb http://ports.ubuntu.com/ trusty-updates main restricted universe multiverse" >> /etc/apt/sources.list
+echo "deb http://ports.ubuntu.com/ trusty-backports main restricted universe multiverse" >> /etc/apt/sources.list
+echo "127.0.0.1 localhost.localdomain localhost" > /etc/hosts
+apt-get -y dist-upgrade >> /home/ubuntu/chroot.log
+apt-get -y update >> /home/ubuntu/chroot.log
+sudo apt-get -f install #fix broken dependencies (may need this for installing python-smbus)
+
+## Packages for installation ##
+## Add addtional packages here and list them on the wiki page ##
+sudo apt-get -y install i2c-tools
+sudo apt-get -y install python-dev
+sudo apt-get -y install python-pip
+sudo apt-get -y install ntp
+sudo apt-get -y install ssh
+sudo apt-get -y install numpy
+sudo apt-get -y install etc
+sudo apt-get -y install perl
+sudo apt-get -y install wpasupplicant
+sudo apt-get -y install linux-firmware
+sudo apt-get -y install linux-firmware-nonfree
+sudo apt-get -y install udhcpc
+sudo apt-get -y install libopencv-dev
+sudo apt-get -y install gpsd
+sudo apt-get -y install python-GPS
+sudo apt-get -y install usbutils
+sudo apt-get -y install python-pyaudio
+#sudo apt-get -y install ntp
+
+## Python libraries ##
+## Add addtional libs here, and list them on the wiki page. ##
+sudo pip -y install Adafruit_Libraries
+#sudo pip install python-smbus
+sudo apt-get -y install python-smbus
+sudo apt-get -y install python-numpy
+
+## Ownership changes. ##
+sudo chown -R root.root /lib/modules/4.0.0-xilinx
+# @jdanner3 additions
+sudo chown -R ubuntu /home/ubuntu
+
+## Install python libs ##
+## Add new installation locations as needed. ##
+cp -r /home/ubuntu/sensorTesting/Adafruit_ADS1x15 /home/ubuntu/ev
+cp -r /home/ubuntu/sensorTesting/Adafruit_Python_BMP-master /home/ubuntu/ev
+cd /home/ubuntu/sensorTesting/Adafruit_Python_BMP-master
+sudo python setup.py install 
+
+## Make segmentation and cleanup files. ##
+cd /home/ubuntu/ev/
+sudo make
+mv segmentation segmentation.arm
+rm *.cpp
+rm *.hpp
+
+addgroup ubuntu i2c
+
+# Folder setup for software/firmware updates
+SOFTWARE="/etc/software_updates"
+mkdir $SOFTWARE
+chown ubuntu:ubuntu $SOFTWARE
+cp /home/ubuntu/cronScripts/getSoftwareUpdate.sh ${SOFTWARE}/.
+cp /home/ubuntu/cronScripts/updateChecker.sh ${SOFTWARE}/.
+# Software Setup Initialization
+SETUP_DIR="/home/ubuntu/setup"
+cp ${SETUP_DIR}/softwaresetup.sh /etc/init.d/.
+cp ${SETUP_DIR}/fstab /etc/.
+cp ${SETUP_DIR}/tools/peek /etc/init.d/.
+cd /etc/rc2.d
+ln -s ../init.d/softwaresetup.sh S99softwaresetup.sh
+# Setup Unique Identifier
+cp ${SETUP_DIR}/setidentity.sh /etc/init.d/.
+cd /etc/rc2.d
+ln -s ../init.d/setidentity.sh S99setidentity.sh
+
+#(crontab -l 2>/dev/null; echo "* * * * * cd /home/ubuntu/cronScripts; /bin/bash /home/ubuntu/cronScripts/sentinel2.sh") | crontab -
+echo "* * * * * ubuntu cd /home/ubuntu/ev; /bin/bash /home/ubuntu/cronScripts/sentinel2.sh" >> /etc/cron.d/healthMon
+echo "#* * * * * ubuntu cd /mnt/ramdisk/apps/ev; /bin/bash /mnt/ramdisk/apps/cronScripts/sentinel2.sh" >> /etc/cron.d/healthMon
+echo "@reboot ubuntu sleep 60 &&  cd /home/ubuntu/ev; /bin/bash /home/ubuntu/cronScripts/devPacket.sh" >> /etc/cron.d/devPacket
+echo "#@reboot ubuntu sleep 60 &&  cd /mnt/ramdisk/apps/ev; /bin/bash /mnt/ramdisk/apps/cronScripts/devPacket.sh" >> /etc/cron.d/devPacket
+echo "#* * * * * ubuntu cd /home/ubuntu/ev; /bin/bash /home/ubuntu/cronScripts/sentinel_script.sh" >> /etc/cron.d/sentinel
+echo "#* * * * * ubuntu cd /mnt/ramdisk/apps/ev; /bin/bash /mnt/ramdisk/apps/cronScripts/sentinel_script.sh" >> /etc/cron.d/sentinel
+echo "#30 5 * * * ubuntu cd /mnt/ramdisk/apps/ev; /usr/bin/python /mnt/ramdisk/apps/cronScripts/health_update.py" >> /etc/cron.d/healthUpdate
+echo "#30 4 * * * root cd ${SOFTWARE}; /bin/bash ${SOFTWARE}/getSoftwareUpdate.sh" >> /etc/cron.d/softwareUpdate
+echo "#30 4 * * * root cd ${SOFTWARE}; /bin/bash ${SOFTWARE}/updateChecker.sh" >> /etc/cron.d/firmwareUpdate
+echo "ubuntu localhost = (root) NOPASSWD: /sbin/reboot" >> /etc/sudoers.d/reboot
+echo "ubuntu localhost = (root) NOPASSWD: /sbin/poweroff" >> /etc/sudoers.d/poweroff
+exit
+
+echo "#####################"
+echo "   EXITING CHROOT"
+echo "#####################"
+sleep 3
+exit
+
---
+++ build_rootfs.sh
@@ -0,0 +1,227 @@
+#!/bin/bash
+
+# Chris Turner
+# cturner48@gatech.edu
+#
+# Script for automating the build process of the GaTech Embedded Systems
+# VIP rootfs. Completes all necessary setup for the development phase
+# of the project. Requires that the SD card be formatted with the
+# BOOT/rootfs partitions as outlined on the team wiki. Will need to 
+# provide mount location of the rootfs partition.
+
+
+## To add new software packages refer to build_chroot.sh. ##
+## Variable Definitions ##
+RFSLOC=0
+CLEAN=0
+VERBOSE=0
+BUILDPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+
+function usage() {
+    echo "Usage: build_rootfs.sh -t=[SD mount location] [options]"
+    echo -e "\t -t|--target: Must provide full mount address of the rootfs SD card partition."
+    echo -e "\t -v|--verbose: Enables verbose mode. [Default 0]"
+   # echo -e "\t -c|--clean: Erases all files on the SD card, and downloads a new copy of the ubuntu core tar."
+    echo -e "\t -h|--help: Show help information. Hint: You're lookin' at it!"
+}
+
+## For loop to check for arguments and assign variables. ##
+for i in "$@"
+do
+case $i in 
+    -t=*|--target=*)
+    RFSLOC="${i#*=}"
+    shift
+    ;;
+    -v|--verbose)
+    VERBOSE=1
+    shift
+    ;;
+    -c|--clean)
+    CLEAN=1
+    shift
+    ;;
+    -h|--help)
+    usage
+    exit 1
+    ;;
+    *)
+    echo "Unkown option $i, now exiting build script."
+    exit 1
+    ;;
+esac
+done
+
+echo "##################################"
+echo "##        BUILDING ROOTFS       ##"
+echo "##################################"
+
+###############################################
+## FORMATING AND PARTITIONING OF SD CARD     ##
+###############################################
+## 		CURRENTLY UNUSED             ##
+###############################################
+
+
+##Check for parted, and Install parted if necessary.
+#
+#{
+#if hash parted 2>/dev/null; then 
+#	echo "parted Installed."
+#else
+#	sudo apt-get install parted
+#
+#fi 
+#}
+#
+##Variable Definitions
+#export OutputDev=sdb
+#Answer=n
+#
+##Devices
+#df -Th
+#read -p "Device to be formated? " OutputDev
+#echo "Seleced Device:" $OutputDev
+#read -p "Is this choice correct? [Y/n] " Answer
+#if [[$Answer = [Yy]];then
+#echo "Formatting " $OutputDev
+
+##Unmount Device
+#sudo umount /dev/"$OutputDev"
+#sudo parted /dev/"$OutputDev" print
+#sudo parted "$@"
+
+###########################################################
+##       DOWNLOADING AND INSTALLING UBUNTU CORE FS       ##
+###########################################################
+
+
+## Download Ubuntu Core ##
+# sudo wget -cv "cdimage.ubuntu.com/ubuntu-core/releases/14.04.3/release/ubuntu-core-14.04.3-core-armhf.tar.gz"
+
+## Copy file to the SD card ##
+# sudo cp ubuntu-core-14.04.3-core-armhf.tar.gz /media/$name/rootfs/
+# sudo rm ubuntu-core-14.04.3-core-armhf.tar.gz
+
+## Change directoy to SD card and unpackage the rootfs ##
+cd $RFSLOC
+if [ "$VERBOSE" -eq 1 ]
+then
+    echo "Unpacking Ubuntu Core FS on SD card..."
+    tar -xzpvf ubuntu-core-14.04.3-core-armhf.tar.gz
+else
+    echo "Unpacking Ubuntu Core FS on SD card..."
+    tar -xzpf ubuntu-core-14.04.3-core-armhf.tar.gz
+fi
+
+## Append and add needed files for accessing the board through UART ##
+
+if [ "$VERBOSE" -eq 1 ]
+then
+    echo "Editing access configuration to include serial ports..."
+fi
+
+## Creating access.conf ##
+cd $RFSLOC/etc/security/
+chmod a=rw access.conf
+echo "# Zynq's UARTs" >> access.conf
+echo "ttyPS0" >> access.conf
+echo "ttyPS1" >> access.conf
+
+if [ "$VERBOSE" -eq 1 ]
+then
+    echo "Editing serial 0 configuration for terminal access..."
+fi
+
+## Creating ttyPS0.conf ##
+cd ../init/
+touch ttyPS0.conf
+chmod a=rw ttyPS0.conf
+echo "# ttyPS0 - getty" > ttyPS0.conf
+echo "#" >> ttyPS0.conf
+echo "# This service maintains a getty on ttyPS0 from the point the system is" >> ttyPS0.conf
+echo "#started until it is shut down again." >> ttyPS0.conf
+echo "" >> ttyPS0.conf
+echo "start on stopped rc RUNLEVEL=[2345] and (" >> ttyPS0.conf
+echo "not-container or" >> ttyPS0.conf
+echo "container CONTAINER=lxc or" >> ttyPS0.conf
+echo "container CONTAINER=lxc-libvirt)" >> ttyPS0.conf
+echo "" >> ttyPS0.conf
+echo "stop on runlevel [!2345]" >> ttyPS0.conf
+echo "" >> ttyPS0.conf
+echo "respawn" >> ttyPS0.conf
+echo "exec /sbin/getty -L -8 115200 ttyPS0" >> ttyPS0.conf
+
+## Adding Xilinx modules to rootfs ##
+if [ "$VERBOSE" -eq 1 ]
+then
+    echo "Unpacking xilinx modules..."
+    mkdir $RFSLOC/lib/modules
+    tar xzvf $BUILDPATH/apps/setup/modules/4.0.0-xilinx.tar.gz -C $RFSLOC/lib/modules/
+else
+    echo "Unpacking xilinx modules..."
+    mkdir $RFSLOC/lib/modules
+    tar xzf $BUILDPATH/apps/setup/modules/4.0.0-xilinx.tar.gz -C $RFSLOC/lib/modules/
+fi
+
+####################################################################
+## PREPARING THE ROOTFS FOR DEVELOPMENT WITH PROJECT INFORMATION  ##
+####################################################################
+
+
+##  Copying all project specific files from the /sw/apps folder excluding setup   ##
+if [ "$VERBOSE" -eq 1 ]
+then
+    echo "Copying all project specific files to rootfs..."
+    echo "All files located under the $BUILDPATH/apps folder are included except the modules folder."
+fi
+
+
+rsync -r --exclude=setup/moduels $BUILDPATH/apps/ $RFSLOC/home/ubuntu
+cp $BUILDPATH/apps/setup/modules/rc.local $RFSLOC/etc/.
+cp $BUILDPATH/apps/setup/modules/interfaces $RFSLOC/etc/network/.
+cp $BUILDPATH/apps/setup/modules/wpa_supplicant.conf $RFSLOC/etc/.
+cp $BUILDPATH/apps/setup/modules/failsafe.conf $RFSLOC/etc/init/.
+cp $BUILDPATH/apps/setup/modules/.bashrc $RFSLOC/home/ubuntu/.
+
+
+## chroot into the rootfs and running the build_chroot.sh script ##
+## Any commands to be run from chroot should be added to build_chroot.sh ##
+## To add new software packages refer to build_chroot.sh. ##
+apt-get install qemu qemu-user-static
+cd $RFSLOC 
+echo "###################"
+echo "# ENTERING CHROOT #"
+echo "###################"
+
+if [ "$VERBOSE" -eq 1 ]
+then
+    echo "Running build_chroot.sh..."
+fi
+
+chmod +x $BUILDPATH/build_chroot.sh
+cp $BUILDPATH/build_chroot.sh $RFSLOC 
+cp /usr/bin/qemu-arm-static usr/bin/
+mv etc/resolv.conf etc/resolv.conf.saved
+cp /etc/resolv.conf etc/resolv.conf
+for m in `echo 'sys dev proc'`; do sudo mount /$m ./$m -o bind; done
+LC_ALL=C chroot . /bin/bash -c "./build_chroot.sh"
+
+if [ "$VERBOSE" -eq 1 ]
+then
+    echo "Leaving chroot and performing cleanup..."
+fi
+
+sleep 3
+for m in `echo 'sys dev proc'`; do sudo umount ./$m; done
+mv etc/resolv.conf.saved etc/resolv.conf
+
+rm -rv $RFSLOC/lib/modules/4.0.0-xilinx/kernel/drivers/net/wireless/rtlwifi
+cp -rv $BUILDPATH/apps/setup/modules/rtl_driver_update/rtlwifi $RFSLOC/lib/modules/4.0.0-xilinx/kernel/drivers/net/wireless/
+cp -v $BUILDPATH/apps/setup/modules/rtl8192cufw_TMSC.bin $RFSLOC/lib/firmware/rtlwifi/
+rm -v $RFSLOC/build_chroot.sh
+
+echo "#######################"
+echo "# END OF BUILD SCRIPT #"
+echo "#######################"
